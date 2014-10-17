edc37075ed7ed890a6755ab06d3ffa151c9a0065
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-10-17 18:55:36 +0700

    add fancybox

---
+++ vendor/assets/javascripts/fancybox-v2.1.5.js
@@ -0,0 +1,4 @@
+//= require fancybox/v2.1.5/jquery.fancybox
+//= require fancybox/v2.1.5/jquery.fancybox-buttons
+//= require fancybox/v2.1.5/jquery.fancybox-thumbs
+//= require fancybox/v2.1.5/jquery.fancybox-media
