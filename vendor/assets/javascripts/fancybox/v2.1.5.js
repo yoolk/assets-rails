9e68a021279041328ab4cba708f18ecb1e1fc502
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-10-22 09:57:20 +0700

    rename fancybox to jquery.fancybox

--- vendor/assets/javascripts/fancybox/v2.1.5.js
+++
@@ -1,4 +0,0 @@
-//= require fancybox/v2.1.5/jquery.fancybox
-//= require fancybox/v2.1.5/jquery.fancybox-buttons
-//= require fancybox/v2.1.5/jquery.fancybox-thumbs
-//= require fancybox/v2.1.5/jquery.fancybox-media
---
+++ vendor/assets/javascripts/jquery.fancybox/v2.1.5.js
@@ -0,0 +1,4 @@
+//= require jquery.fancybox/v2.1.5/jquery.fancybox
+//= require jquery.fancybox/v2.1.5/jquery.fancybox-buttons
+//= require jquery.fancybox/v2.1.5/jquery.fancybox-thumbs
+//= require jquery.fancybox/v2.1.5/jquery.fancybox-media
