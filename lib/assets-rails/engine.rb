5a8d144feb9c5e6b9f52a84d3b8d499c7aede51f
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-12-31 00:43:37 +0700

    update precompile

--- lib/assets-rails/engine.rb
+++ lib/assets-rails/engine.rb
@@ -3,7 +3,9 @@ require 'rails/engine'
 module Assets
   module Rails
     class Engine < ::Rails::Engine
-
+      initializer 'assets-rails.precompile' do |app|
+        app.config.assets.precompile += [ Proc.new { |path, fn| fn =~ /assets-rails\/vendor\/assets/ && !%w(.js .css).include?(File.extname(path)) } ]
+      end
     end
   end
 end
