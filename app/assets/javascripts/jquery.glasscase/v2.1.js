8f03375b8848a0ae3a7f5efa6872033f7a5ad1d9
Vuthy San <saravuthy@gmail.com>
2015-10-01 12:01:18 +0700

    add libraries glasscase and wow

---
+++ app/assets/javascripts/jquery.glasscase/v2.1.js
@@ -0,0 +1,2 @@
+//= require jquery.glasscase/v2.1/modernizr.custom.js
+//= require jquery.glasscase/v2.1/jquery.glasscase.min.js
---
+++ app/assets/javascripts/wow/v1.1.2.js
@@ -0,0 +1,2 @@
+// https://github.com/matthieua/WOW/blob/master/dist/wow.js
+//= require wow/v1.1.2/wow.js
